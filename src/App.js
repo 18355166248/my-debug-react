49cf9b768c79a843251ca17281aed55bd14070b4
江浪 <643546122@qq.com>
2022-02-25 00:01:45 +0800

    feat: 11

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ function App() {
   const [txt, setTxt] = useState(1);
 
   return (
-    <div className="App" onClick={() => setTxt(6)}>
-      {txt}
+    <div className="App">
+      <div onClick={() => setTxt(2)}>2-{txt}</div>
+      <div onClick={() => setTxt(3)}>3-{txt}</div>
+      <div onClick={() => setTxt(4)}>4-{txt}</div>
     </div>
   );
 }
